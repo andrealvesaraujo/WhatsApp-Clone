798791ab04fefc8d5ed539006951ba71f24add7c
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-30 16:41:54 -0300

    W04 - Eventos para abrir/fechar os painéis "Editar Perfil" e "Adicionar Contato"

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     
     }
 
@@ -70,6 +71,53 @@ class WhatsAppController {
             this.el[Format.getCamelCase(element.id)] = element;
             
         });
+
+    }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                
+                this.el.panelEditProfile.addClass('open');
+
+            },300);
+
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{                
+                
+                this.el.panelAddContact.addClass('open');
+
+            },300);
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        });
+
+    }
+
+    closeAllLeftPanel() {
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
 
     }
 
